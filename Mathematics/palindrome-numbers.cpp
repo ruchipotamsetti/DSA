761cc0867146e5156eddc60944fe7ecf9fe00f19
Ruchita Potamsetti <70581620+ruchipotamsetti@users.noreply.github.com>
2021-05-13 02:39:00 +0530

    Update palindrome-numbers.cpp

--- Mathematics/palindrome-numbers.cpp
+++ Mathematics/palindrome-numbers.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 bool palindrome_number(int n)
 {
+    int original = n;
     int digit = n % 10;
-    int palindrome = digit;
+    long palindrome = digit;
     while(n / 10 != 0)
     {
         n = n / 10;
         digit = n % 10;
         palindrome = (palindrome * 10) + digit;
     }
-    return n == palindrome;
+    cout<<palindrome;
+    return original == palindrome;
 
 }
 
 int main(){
-    int n;
+    long n;
     cin>>n;
     palindrome_number(n) ? printf("\nYES") : printf("\nNO");
     return 0;
