b35cc3a16349478b7090ea6a7a24b34011aeb451
Ruchita Potamsetti <70581620+ruchipotamsetti@users.noreply.github.com>
2021-05-15 00:46:18 +0530

    Add files via upload

---
+++ Recursion/josephus-problem.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int josephus(int n, int k)
+{
+    if(n == 1){
+        return 0;
+    }
+    return (josephus(n-1, k) + k)% n;
+}
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int result = josephus(n, k);
+    cout<<result;
+    return 0;
+}
+
